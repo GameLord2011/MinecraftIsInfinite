1645adc27c9bea5a09fdbbe5349b1c3bc0ec1181
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2026-06-01 13:46:21 -0400

    Removed commented out code and example mixin.

--- src/client/java/dev/gamelord2011/client/mixin/WorldBorderRendererMixin.java
+++ src/client/java/dev/gamelord2011/client/mixin/WorldBorderRendererMixin.java
@@ -1,21 +1,9 @@
 package dev.gamelord2011.client.mixin;
 
-// import dev.gamelord2011.MinecraftIsInfinite;
-
-// import java.util.OptionalDouble;
-// import java.util.OptionalInt;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-// import com.mojang.blaze3d.pipeline.RenderTarget;
-// import com.mojang.blaze3d.systems.RenderPass;
-// import com.mojang.blaze3d.systems.RenderSystem;
-// import com.mojang.blaze3d.textures.GpuTextureView;
-
-// import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.WorldBorderRenderer;
 import net.minecraft.client.renderer.state.level.WorldBorderRenderState;
 import net.minecraft.world.phys.Vec3;
